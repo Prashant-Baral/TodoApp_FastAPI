dceb748bb6bc9352dda070f74a22f06c0bfcde61
prashant <baralprashant09@gmail.com>
2025-12-07 11:59:46 +0530

    Updated the data validation for the description of the todo to 300 chrs.

--- to_do_app/routers/todos.py
+++ to_do_app/routers/todos.py
@@ -32,7 +32,7 @@ user_dependency = Annotated[dict, Depends(get_current_user)]
 
 class TodoRequest(BaseModel):
     title: str = Field(min_length=3)
-    description: str = Field(min_length=3, max_length=100)
+    description: str = Field(min_length=3, max_length=300)
     priority: int = Field(gt=0, lt=6)
     complete: bool
 
